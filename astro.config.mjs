7c3388fba5cf0578ba7c93b44e70b5ace96013e9
Victor Lantigua <lantiguav@gmail.com>
2024-09-02 11:01:41 -0400

    feat: enabble vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,9 @@ import vercel from '@astrojs/vercel/serverless'
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
   image: {
     service: passthroughImageService(),
   },
