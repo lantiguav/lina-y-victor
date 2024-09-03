0638c9b700dba7bb5bf1de27f45d8a4ec8feafea
Victor Lantigua <lantiguav@gmail.com>
2024-09-03 18:30:10 -0400

    feat: integrate sentry

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,26 @@
 import { defineConfig, passthroughImageService } from 'astro/config'
-
 import vercel from '@astrojs/vercel/serverless'
+
+import sentry from '@sentry/astro'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel({
-    webAnalytics: { enabled: true }
+    webAnalytics: {
+      enabled: true,
+    },
   }),
   image: {
     service: passthroughImageService(),
   },
+  integrations: [
+    sentry({
+      dsn: 'https://ca49aa683b1a4c8d6a03df744ea5efe5@o4507891270746112.ingest.us.sentry.io/4507891276840960',
+      sourceMapsUploadOptions: {
+        project: 'javascript-astro',
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      },
+    }),
+  ],
 })
